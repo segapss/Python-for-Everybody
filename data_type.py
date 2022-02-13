46559e93e8cfba43e7cbe6754aef1a00e7526e1a
sungsoo.park <sungsoo.park@outlook.kr>
2022-02-13 16:13:21 +0100

    Data type

---
+++ data_type.py
@@ -0,0 +1,16 @@
+#Number
+##Integer
+a = 123
+a = -178
+a = 0
+#Float
+a = 1.2
+a = -3.45
+a = 4.24E10
+a = 4.24e-10
+#Octal, Hexadecimal
+a = 0o177, a = 0x8ff
+#Arithmetry(+.-.*,/)
+#a ** b ex) 3 ** 4 =81
+#Remainder return  7 % 3 = 1   3 % 7 = 3
+#Quotient return  7 // 4 = 1 
